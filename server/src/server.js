0c61d59a5558832b806e405c7c39ec9349661ada
aymaneatigui <atigui.cs@gmail.com>
2024-08-20 15:02:34 +0100

    chore: Update server.js with proper formatting and imports

--- server/src/server.js
+++ server/src/server.js
@@ -1,37 +1,32 @@
-const expresse = require("express")
-require("dotenv").config()
+const expresse = require("express");
+require("dotenv").config();
 const cors = require("cors");
-const mongoose= require("mongoose")
-const postJob= require("./Routes/jobPosts")
-const userRoute= require("./Routes/userRoute")
-const appliRoute= require("./Routes/application")
-const internRoute= require("./Routes/Internships")
-const app= expresse()
+const mongoose = require("mongoose");
+const postJob = require("./Routes/jobPosts");
+const userRoute = require("./Routes/userRoute");
+const appliRoute = require("./Routes/application");
+const internRoute = require("./Routes/Internships");
+const app = expresse();
 
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+app.use(cors());
 
-mongoose.connect(process.env.MONG_URI)
-.then(()=>{
-        app.listen(process.env.PORT,()=>{
-            console.log("listening on ",process.env.PORT)
-        })
-    }
-)
-.catch((error)=>{
-    console.log("Error:",error)
-})
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening on ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
 
-app.use(expresse.json())
-app.use((req,res,next)=>{
-    console.log(req.method,req.path)
-    next()
-})
-app.use("/posts",postJob)
-app.use("/users",userRoute)
-app.use("/internships",internRoute)
-app.use("/applications",appliRoute)
-
-
+app.use(expresse.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+app.use("/posts", postJob);
+app.use("/users", userRoute);
+app.use("/internships", internRoute);
+app.use("/applications", appliRoute);
